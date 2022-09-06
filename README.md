a3ee5f54c5b2f3fd6e7cfdbd95319330d2998ed9
Vincent Sjögren <vincentsjogren@gmail.com>
2022-09-06 12:08:12 +0200

    updated gitignore, added readme, added license and updated exported functions

---
+++ README.md
@@ -0,0 +1,20 @@
+# x-is-equal
+
+`x-is-equal` is a package for checking equality of multiple values
+
+Package is bundled using [microbundle](https://www.npmjs.com/package/microbundle)
+
+## Installation
+
+`npm i x-is-equal`
+
+### In Node.js
+
+```js
+// ESM:
+import { isEqual, isEqualArr, isEqualObj } from 'x-is-equal';
+// CommonJS:
+const x = require('x-is-equal');
+// or:
+const { isEqual, isEqualArr, isEqualObj } = require('x-is-equal');
+```
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,87 @@
-// import {isArr, isObj} from "x-is-type";
-export type equalityChecker = (...args) => boolean;
-export type argsComparer = (a, b) => boolean;
+import { isArr, isObj, isDate } from 'x-is-type';
+
+export type equalityChecker = (...args: any[]) => boolean;
+export type argsComparer = (a: any, b: any) => boolean;
 export type argsValidator = equalityChecker;
 
-const createEqualityChecker = (
-  argsComparerFn: argsComparer,
-  validateArgsFn?: argsValidator
-): equalityChecker => {
-  return function (...args): boolean {
-    if (args.length < 2) return true;
-    if (isFn(validateArgsFn) && !validateArgsFn(...args)) return false;
-    const comparer = isFn(argsComparerFn) ? argsComparerFn : (a, b) => a === b;
-    for (let i = 1; i < args.length; i++) {
-      const a = args[i - 1],
-        b = args[i];
-      if (comparer(a, b)) continue;
-      return false;
-    }
-    return true;
-  };
-};
+function createEqualityChecker(
+	validateArgsFn: argsValidator | null = null,
+	argsComparerFn: argsComparer | null = null
+) {
+	return function (...args: any[]): boolean {
+		if (args.length < 2) return true;
+		if (typeof validateArgsFn === 'function' && !validateArgsFn(...args)) {
+			return false;
+		}
+		const comparer =
+			typeof argsComparerFn === 'function'
+				? argsComparerFn
+				: (a: any, b: any) => a === b;
+		for (let i = 1; i < args.length; i++) {
+			const a = args[i - 1],
+				b = args[i];
+			if (comparer(a, b)) continue;
+			return false;
+		}
+		return true;
+	};
+}
 
 const isEqualConstructor = createEqualityChecker(
-  (a, b) => a.constructor.name === b.constructor.name
+	null,
+	(a, b) => a?.constructor?.name === b?.constructor?.name
 );
 
-export { isEqualConstructor };
+const isEqualDate = createEqualityChecker(
+	isDate,
+	(a, b) => Number(a) === Number(b)
+);
+
+/**
+ * Determine if multiple arrays contain equal values recursively
+ */
+function isEqualArr(...args: any[]): boolean {
+	return createEqualityChecker(isArr, (a: any[], b: any[]) => {
+		if (a.length !== b.length) return false;
+		for (let i = 0; i < a.length; i++) {
+			if (a[i] === b[i]) continue;
+			if (isEqualArr(a[i], b[i])) continue;
+			if (isEqualObj(a[i], b[i])) continue;
+			return false;
+		}
+		return true;
+	})(...args);
+}
+/**
+ * Determine if multiple objects contain equal values recursively
+ */
+function isEqualObj(...args: any[]): boolean {
+	return createEqualityChecker(
+		(...args) => isObj(...args) && isEqualConstructor(...args),
+		(a: Object, b: Object) => {
+			const keys = { a: Object.keys(a), b: Object.keys(b) };
+			if (keys.a.length !== keys.b.length) return false;
+			for (const key of keys.a) {
+				if (a[key] === b[key]) continue;
+				if (isEqualArr(a[key], b[key])) continue;
+				if (isEqualObj(a[key], b[key])) continue;
+				if (isEqualDate(a[key], b[key])) continue;
+				return false;
+			}
+			return true;
+		}
+	)(...args);
+}
+
+/**
+ * Determine if multiple values are equal,
+ * works recursively on arrays and objects.
+ */
+function isEqual(...args: any[]): boolean {
+	return createEqualityChecker(
+		null,
+		(a, b) => a === b || isEqualObj(a, b) || isEqualArr(a, b)
+	)(...args);
+}
+
+export { isEqual, isEqualConstructor, isEqualArr, isEqualObj };
