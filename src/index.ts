d813ce1f6585b0b873f51f4145ad259cbc8102ab
Vincent Sjögren <vincentsjogren@gmail.com>
2022-12-07 12:39:49 +0100

    updated core functionality to only accept two parameters and cleaned up code

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,36 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { isArr, isObj } from 'x-is-type';
 
-export type equalityChecker = (...args: unknown[]) => boolean;
-export type argsComparer = (a: any, b: any) => boolean;
-export type argsValidator = equalityChecker;
-
-type obj = Record<string | number | symbol, unknown>;
-
-function checkEquality(args: any[], comparer: (a: any, b: any) => boolean) {
-    if (args.length < 2) return true;
-    const type = typeof args[0];
-    if (!args.every((value) => typeof value === type)) return false;
-    for (let i = 1; i < args.length; i++) {
-        if (args[i - 1] === args[i]) continue;
-        if (!comparer(args[i - 1], args[i])) return false;
-    }
-    return true;
-}
-
 /**
- * Determine if multiple values are arrays and contain equal values recursively
- */
-function isEqualArr(...args: any[]) {
-    if (!isArr(...args)) return false;
-    return checkEquality(args, (a: any[], b: any[]) => {
-        if (a.length !== b.length) return false;
-        for (let i = 0; i < a.length; i++) {
-            const equal =
-                a[i] === b[i] ||
-                (a[i] === a && b[i] === b) ||
-                isEqualArr(a[i], b[i]) ||
-                isEqualObj(a[i], b[i]);
-            if (!equal) return false;
-        }
-        return true;
-    });
-}
-/**
- * Determine if multiple values are objects and contain equal values recursively
- * Ignores object constructor, just checks enumerable values
- */
-function isEqualObj(...args: any[]) {
-    if (!isObj(...args)) return false;
-    return checkEquality(args, (a: obj, b: obj) => {
-        const keys = { a: Object.keys(a), b: Object.keys(b) };
-        if (keys.a.length !== keys.b.length) return false;
-        for (const k of keys.a) {
-            const equal =
-                k in b &&
-                (a[k] === b[k] ||
-                    (a[k] === a && b[k] === b) ||
-                    isEqualArr(a[k], b[k]) ||
-                    isEqualObj(a[k], b[k]));
-            if (!equal) return false;
-        }
-        return true;
-    });
-}
-
-/**
- * Determine if multiple values are equal,
+ * Determine if a and b are equal,
  * strict equal checks are performed on primitive types and
  * deep equal checks are performed on arrays and objects.
  */
-function isEqual(...args: any[]) {
-    return checkEquality(args, (a, b) => isEqualArr(a, b) || isEqualObj(a, b));
+export function isEqual(a: unknown, b: unknown): boolean {
+    return a === b || isEqualArr(a, b) || isEqualObj(a, b);
 }
 
-export { isEqual, isEqualArr, isEqualObj };
+/** Recursively determine if two values are arrays and contain equal values. */
+export function isEqualArr(a: unknown, b: unknown) {
+    if (!isArr(a) || !isArr(b) || a.length !== b.length) return false;
+    if (a === b) return true;
+    for (let i = 0; i < a.length; i++) {
+        if (!isEqual(a[i], b[i])) return false;
+    }
+    return true;
+}
+/** Recursively determine if two values are objects and contain equal enumerable values. */
+export function isEqualObj(a: unknown, b: unknown) {
+    if (!isObj(a) || !isObj(b)) return false;
+    if (a === b) return true;
+    const keys = {
+        a: Object.keys(a),
+        b: Object.keys(b),
+    };
+    if (keys.a.length !== keys.b.length) return false;
+    for (const key of keys.a) {
+        if (!(key in b) || !isEqual(a[key], b[key])) {
+            return false;
+        }
+    }
+    return true;
+}
